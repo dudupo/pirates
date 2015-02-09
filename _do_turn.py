1c1f533c6b1d04b9367e5af044ee6297043e2982
Nicodemes <sasha@paticon.com>
2015-02-09 14:15:25 +0200

    fix the import

--- _do_turn.py
+++ _do_turn.py
@@ -1,10 +1,12 @@
 
-import importlib
+from importlib import import_module
 import emulator
+DO_TURN_METHOD_NAME= 'do_turn'
 class _do_turn:
-	def __init__(self, name_of_bot):	
-		importlib.import_module(name_of_bot)
-		self.do_turn = name_of_bot.do_turn
+	'A class that represents the function "do_turn" in each bot'
+	def __init__(self, name_of_bot):
+		mod = import_module(name_of_bot)
+		self.do_turn = getattr(mod ,DO_TURN_METHOD_NAME)
 
 	def __call__():
 		self.do_turn()
