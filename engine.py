8e7a2874c24f235bad49bed28eb6f961e0fd2275
Nicodemes <sasha@paticon.com>
2015-02-16 16:43:54 +0200

    jh

--- engine.py
+++ engine.py
@@ -58,9 +58,6 @@ def init(playernames):
 		score_table[player] = 0
 	outPut.init_drawing()
 
-	for pirate in pirates:
-		outPut.print_change_pirate_location(pirate)
-
 
 
 def bind_pirate(pirate):
@@ -70,8 +67,6 @@ def bind_pirate(pirate):
 	pirates.append(pirate)
 	living_pirates.append(pirate)
 	drawmap[(pirate.location.x,pirate.location.y)]=pirate.player.sign
-	
-
 
 def spawn_pirate(pirate):
 	print("spawning {} ".format(pirate))
@@ -104,6 +99,11 @@ def move(tasks):
 		wantstomove[pirate]=False
 	for (t, pirate, ad) in tasks:
 		wantstomove[pirate]=True    
+		#for (e, comper, bd)in tasks:
+		#	if pirate!=comper:
+		#		if pirate.location+ad==comper.location+bd:
+		#			didnt_move[pirate]=False
+		#			didnt_move[comper]=False
 	
 	for (taskname, pirate, direaction) in tasks:
 			newlocation = pirate.location+direaction
@@ -113,7 +113,6 @@ def move(tasks):
 					pirate.location = newlocation
 					drawmap[(pirate.location.x,pirate.location.y)]=last
 
-					pirate.current_direaction = "0"
 					outPut.print_change_pirate_location(pirate) 
 					
 			else:
@@ -160,7 +159,6 @@ def update():
 	
 	try_to_revive()
 	move(tasks)
-
 	battle()
 	
 	cap()
@@ -197,6 +195,3 @@ def draw():
 	f.write((todorw)+"\n\n\n")
 	f.close()
 	debug_turn_message=""
-
-
-		
