63846425a90ba90f4dafd6cdea7da54c87c51034
dudupo <dudupo51@gmail.com>
2015-02-20 17:15:41 +0200

    20/02

--- bots/dani.py
+++ bots/dani.py
@@ -1,6 +1,7 @@
 import random
 def do_turn(game):
-	
+
+
 	if len(game.get_not_my_islands())<=0:
 		return
 	for pirate in game.get_my_pirates():
@@ -15,7 +16,12 @@ def do_turn(game):
 		if target != None:
 			d = game.get_directions(pirate,target)
 			if len(d) > 0:
-				game.set_sail(pirate,d[0])
+				ret = game.set_sail(pirate,d[0])
+				if not ret :
+					if d[0] == "e" or "w":
+						game.set_sail(pirate,random.choice(["n" ,"s"]))
+					else :
+						game.set_sail(pirate,random.choice(["e" ,"w"]))
 			else:
 				continue
 		
